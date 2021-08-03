3bf16a1484a358e800f44731a6853f66d0ec78c7
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-08-02 18:34:09 -0700

    hotfix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
-fs.writeFile('index1.html', generatePage(name, github), err => {
+fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
 
     console.log('Portfolio complete! Check out index.html to see the output!')
